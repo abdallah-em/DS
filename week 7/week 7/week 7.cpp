b015539312229db29be94c162a353315b82c5a32
Abdallah-em <omdaabood@gmail.com>
2020-07-03 18:42:34 +0200

    Node Class

--- week 7/week 7/week 7.cpp
+++ week 7/week 7/week 7.cpp
@@ -1,68 +1,11 @@
 ﻿#include <iostream>
+#include"Node.h"
 using namespace std;
 
 int main(){
 	
-	//first test:    (Address)
-
-	int x;
-	x = 1993;
-	int* ptr;
-	ptr = &x;
-	cout << *ptr << endl;
-	
-  
-
-	//second test pointer :    (arr = pointer)
-
-	int arr[3] = { 2 , 4 , 7 };
-	cout << *arr<<' ';
-	cout << *(arr + 1)<<' ';
-	cout << *(arr + 2)<<' ';
-
-	
-
-	//third test :        (direct poinetr)
-
-	int* ptr = new int;
-	*ptr = 10;
-	cout << *ptr << endl;
-
-	double* salary = new double;
-	*salary = 1500;
-	cout << *salary << endl;
-
-	int* arr = new int[3];
-	*ptr = 7;
-	*(ptr + 1) = 9;
-	*(ptr + 2) = 11;
-	for (int i = 0; i < 3; i++)
-		cout << ptr[i] << ' ';
-	cout << endl;
-	
-	
-	//fourth test :    (re5em gedn)
-
-	int** arr = new int* [50];
-	for (int i = 0; i < 50; i++)
-		arr[i] = new int[20];
-	arr[5][10] = 500;
-	cout << arr[5][10] << endl;
-	
-
-	//fifth test :           (delete pointer)
-	int* ptr = new int;
-	*ptr = 10;
-	cout << *ptr << endl;
-
-	delete ptr;
-
-	cout << *ptr;
-	cout << "abdalalh" << endl;
-
-	//ok ;
-
-      
+	Node node(5);
+	cout << node.value;
 
      return 0;
 }
