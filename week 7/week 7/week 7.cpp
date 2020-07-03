082f08604fbb05559d0591119a1bf8b473c97a13
Abdallah-em <omdaabood@gmail.com>
2020-07-03 17:16:02 +0200

    third test

--- week 7/week 7/week 7.cpp
+++ week 7/week 7/week 7.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main(){
-
- //first test:    (Address)
+	/*
+    //first test:    (Address)
 
 	int x;
 	x = 1993;
@@ -18,6 +18,25 @@ int main(){
 	cout << *arr<<' ';
 	cout << *(arr + 1)<<' ';
 	cout << *(arr + 2)<<' ';
+	*/
+
+	//third test :        (direct poinetr)
+
+	int* ptr = new int;
+	*ptr = 10;
+	cout << *ptr << endl;
+
+	double* salary = new double;
+	*salary = 1500;
+	cout << *salary << endl;
+
+	int* arr = new int[3];
+	*ptr = 7;
+	*(ptr + 1) = 9;
+	*(ptr + 2) = 11;
+	for (int i = 0; i < 3; i++)
+		cout << ptr[i] << ' ';
+	cout << endl;
 
 
 	return 0;
