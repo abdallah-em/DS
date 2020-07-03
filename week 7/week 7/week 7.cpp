e2032c7bef2629c0c9ab3cb0e3e70ebd8b711f44
Abdallah-em <omdaabood@gmail.com>
2020-07-03 16:51:37 +0200

    second test

--- week 7/week 7/week 7.cpp
+++ week 7/week 7/week 7.cpp
@@ -1,15 +1,23 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
+
+ //first test:    (Address)
+
 	int x;
 	x = 1993;
 	int* ptr;
 	ptr = &x;
-	cout << *ptr;
+	cout << *ptr << endl;
+  
 
+	//second test pointer :    (arr = pointer)
 
+	int arr[3] = { 2 , 4 , 7 };
+	cout << *arr<<' ';
+	cout << *(arr + 1)<<' ';
+	cout << *(arr + 2)<<' ';
 
 
 	return 0;
